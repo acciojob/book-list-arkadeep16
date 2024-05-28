2f6c03be4ae9d262ea90f9246e5b233f0490c5d6
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-05-29 02:14:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const addBtn = document.getElementById("submit");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const tBody = document.getElementById("book-list");
+addBtn.addEventListener("click", (event)=>{
+	event.preventDefault();
+	let titleVal = title.value;
+	let authorVal = author.value;
+	let isbnVal = isbn.value;
+	// console.log(titleVal,authorVal,isbnVal);
+
+	let tr = document.createElement("tr");
+	
+	tr.innerHTML = `<tr>
+				<td>${titleVal}</td>
+				<td>${authorVal}</td>
+				<td>${isbnVal}</td>
+				<td></td>
+				</tr>`
+	tBody.append(tr);
+	let deleteBtn = document.createElement("button");
+	deleteBtn.innerText ="X";
+	deleteBtn.classList.add= "delete";
+	deleteBtn.style.backgroundColor = "red";
+	deleteBtn.style.color = "white";
+	
+	tr.append(deleteBtn)
+
+
+	deleteBtn.addEventListener("click", ()=>{
+		tr.remove();
+	})
+})
